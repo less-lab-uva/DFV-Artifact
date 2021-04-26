cf12045820c76826f498f76deee1d96e337368de
Felipe Toledo <felipetoledo4815@gmail.com>
2021-04-26 09:45:25 -0300

    Fixed falsify_ls.sh in RQ2

--- rq2/falsify_ls.sh
+++ rq2/falsify_ls.sh
@@ -4,7 +4,7 @@ for std in 0.25 0.5 0.75 1 1.25 1.5 1.75 2 2.25 2.5 2.75 3 3.25 3.5 3.75 4; do
     mkdir -p ./output/ls_study/std$std
     for ((run = 0 ; run < 5 ; run++)); do
         mkdir -p ./output/ls_study/std$std/run$run
-        for ((property = 2 ; property < 3 ; property++)); do
+        for ((property = 1 ; property < 3 ; property++)); do
             mkdir -p ./output/ls_study/std$std/run$run/property$property
             python ./common/dnnf_pipeline_ls_study.py \
                 ./saved_models/network \
